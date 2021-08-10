523370b2ae936a191d8c4030e087b756ff06e9ae
Chekwube <chekwube@rubikpay.net>
2021-08-10 01:22:39 +0100

    refactored customer controller

--- src/@universal/interfaces/request.interface.ts
+++
@@ -1,6 +0,0 @@
-// import { Request } from 'express';
-// import { ICustomer } from './customer.interface';
-
-// export interface RequestWithCustomer extends Request {
-//   customer: ICustomer;
-// }
--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,24 +1,23 @@
-import { NextFunction, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AccountOpenDTO } from './customer.dto';
-import { RequestWithCustomer } from '@/@universal/interfaces/request.interface';
 import CustomerService from './customer.service';
 import UniversalController from '@/@universal/controller/universal.controller';
 
 class CustomerController extends UniversalController {
   public customerService = new CustomerService();
 
-  public openAccount = async (req: RequestWithCustomer, res: Response, next: NextFunction): Promise<void> => {
+  public openAccount = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const { customer, body } = req;
+      const { body } = req;
       const userData: AccountOpenDTO = body;
-      const response = await this.customerService.processAccountOpening(customer, userData);
+      const response = await this.customerService.processAccountOpening(req['customer'], userData);
       await this.controllerResponseHandler(response, req, res);
     } catch (error) {
       await this.controllerErrorHandler(req, res, error);
     }
   };
 
-  public getCustomerByAccountNumber = async (req: RequestWithCustomer, res: Response, next: NextFunction): Promise<void> => {
+  public getCustomerByAccountNumber = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { params } = req;
       const userData: string = params.accountNumber;
